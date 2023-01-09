33f62a8148869bd58ddfe4d934d5d88f9228da10
jeremyhuang035 <119722002+jeremyhuang035@users.noreply.github.com>
2023-01-10 09:15:43 +1000

    fixed import module error in index.js

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
 import Head from 'next/head';
 import Link from 'next/link';
-import Layout, { siteTitle } from '../components/layout';
+import Layout, { siteTitle } from '../components/Layout';
 import { getSortedPostsData } from '../lib/posts';
 import utilStyles from '../styles/utils.module.css';
-import Date from '../components/date';
+import Date from '../components/Date';
 
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData();
